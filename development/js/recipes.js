cb62608dc10add4266d015511c380df3974e6ff6
ajek1993@gmail.com <ajek1993@gmail.com>
2023-07-31 23:21:39 +0200

    fix display name on page

--- development/js/recipes.js
+++ development/js/recipes.js
@@ -1,5 +1,10 @@
+const userNameOnPage = document.querySelector(".user-name");
 const navItems = document.querySelector(".nav-bar__list");
 
 if (document.location.href === navItems.children[1].children[0]["href"]) {
   navItems.children[1].classList.add("chosen_item");
+}
+
+if (localStorage.hasOwnProperty("name")) {
+  userNameOnPage.innerText = localStorage.getItem("name");
 }
