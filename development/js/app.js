e3f6dd9a867f97b5816257a559164ecb860c9da6
Tommatino <tomowczarski@gmail.com>
2023-08-04 22:38:11 +0200

    4.2 record of recipe to localStorage

--- development/js/app.js
+++ development/js/app.js
@@ -44,6 +44,51 @@ addRecipeBtn.addEventListener("click", () => {
   secondWindow.classList.add("hide");
 });
 
+//ADD RECIPE 4.2 TOMEK, PRÓBA ZAPISU PRZEPISU DO LOCAL STORAGE
+
+const recipeSaveButton = document.querySelector(".schedule_recipe_header .save_btn")
+let allRecipies = []
+function Recipe(id, title, description) {
+  this.id = id; // id przepisu
+  this.title = title; // nazwa przepisu
+  this.description = description; // opis przepisu
+  this.instructions = []; // instrukcje przepisu
+  this.ingredients = []; // składniki przepisu
+}
+
+const saveToLocalStorage = newObject => {
+  if (localStorage.getItem("recipes") != null) {
+    allRecipies  = JSON.parse(localStorage.getItem("recipes"));
+    allRecipies.push(newObject);
+    localStorage.setItem("recipes", JSON.stringify(allRecipies));
+  } else {
+    allRecipies .push(newObject);
+    localStorage.setItem("recipes", JSON.stringify(allRecipies));
+  }
+  alert("Przepis zapisany do localStorage");
+}
+
+recipeSaveButton.addEventListener("click", function (){
+  const recipeName = document.querySelector("input[placeholder=\"Wpisz nazwę przepisu\"]")
+  const recipeDescription = document.querySelector("textarea[placeholder=\"Opisz swój przepis\"]")
+  const instructions = document.querySelectorAll(".instructions__list li")
+  const ingredients = document.querySelectorAll(".recipes__list li")
+  const instructionsArray = Array.from(instructions).map(li => li.innerText)
+  const ingredientsArray = Array.from(ingredients).map(li => li.innerText)
+
+  const newRecipe = new Recipe(allRecipies.length + 1, recipeName.value, recipeDescription.value)
+  newRecipe.instructions = [...instructionsArray]
+  newRecipe.ingredients = [...ingredientsArray]
+
+  saveToLocalStorage(newRecipe)
+
+  recipeName.value = "";
+  recipeDescription.value = "";
+  instructions[0].parentElement.innerHTML = "";
+  ingredients[0].parentElement.innerHTML = "";
+})
+
+
 //ADD SCHEDULE
 
 const ScheduleBox = document.querySelector(".add_new_schedule");
